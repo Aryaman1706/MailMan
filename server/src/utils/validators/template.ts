49614aab93770169bc03a27f3ba3f30de60ca520
Aryaman1706 <agrover1_be19@thapar.edu>
2021-05-11 20:46:11 +0530

    fix

--- server/src/utils/validators/template.ts
+++ server/src/utils/validators/template.ts
@@ -7,16 +7,27 @@ export const newTemplate = (
   title: string;
   html: string;
   subject: string;
-  format: { email: string; [key: string]: string };
+  format: string;
 }> => {
   const schema = Joi.object({
     title: Joi.string().trim().required(),
     subject: Joi.string().trim().required(),
     html: Joi.string().required(),
-    format: Joi.object({
-      email: Joi.string().trim().length(1),
-    }).pattern(Joi.string().trim(), Joi.string().trim().length(1)),
+    format: Joi.string().required(),
   });
+
+  return schema.validate(body);
+};
+
+export const validateFormat = (
+  body: any
+): CustomValidationResult<{
+  email: string;
+  [k: string]: string;
+}> => {
+  const schema = Joi.object({
+    email: Joi.string().trim().length(1).uppercase().required(),
+  }).pattern(Joi.string().trim(), Joi.string().trim().length(1).uppercase());
 
   return schema.validate(body);
 };
