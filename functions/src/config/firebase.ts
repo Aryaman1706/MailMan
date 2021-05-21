6f88121ed40ef041c49803a0c32346890fec8f2b
Aryaman1706 <agrover1_be19@thapar.edu>
2021-05-21 15:04:20 +0530

    deploy prep

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,17 +1,6 @@
 import admin from "firebase-admin";
-import path from "path";
 
-const serviceAccount = path.resolve(
-  __dirname,
-  "../",
-  "../",
-  "av-mailer-396d7-firebase-adminsdk-m7d0y-8881d8f398.json"
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "av-mailer-396d7.appspot.com",
-});
+admin.initializeApp();
 
 export const collections = {
   user: "user",
